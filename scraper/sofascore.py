bf8262ee930a20d8df3a0218d20f6a85586b3bb6
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-11-30 00:38:17 +0300

    Update sofascore.py

--- scraper/sofascore.py
+++ scraper/sofascore.py
@@ -1,16 +1,9 @@
 import requests
 
 BASE_URL = "https://api.sofascore.com/api/v1"
-
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+headers = {"User-Agent": "Mozilla/5.0"}
 
 def get_team_id_by_name(team_name: str):
-    """
-    Sofascore'da takım adını arayıp ID döndürür.
-    Örn: get_team_id_by_name("konyaspor") → 6034 gibi.
-    """
     url = f"{BASE_URL}/search?q={team_name}"
     resp = requests.get(url, headers=headers)
 
@@ -19,8 +12,7 @@ def get_team_id_by_name(team_name: str):
 
     data = resp.json()
 
-    results = data.get("results", [])
-    for item in results:
+    for item in data.get("results", []):
         if item.get("entity") == "team":
             if team_name.lower() in item["name"].lower():
                 return item["id"]
