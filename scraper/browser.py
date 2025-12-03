f41ce23ae76847078e01f21ae00b0af3848793e6
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-12-03 19:58:57 +0300

    Update browser.py

--- scraper/browser.py
+++ scraper/browser.py
@@ -1,20 +1,12 @@
 from playwright.async_api import async_playwright
 
 class Browser:
-    def __init__(self):
-        self.playwright = None
-        self.browser = None
-        self.context = None
-        self.page = None
-
     async def __aenter__(self):
         self.playwright = await async_playwright().start()
         self.browser = await self.playwright.chromium.launch(headless=True)
-        self.context = await self.browser.new_context()
-        self.page = await self.context.new_page()
-        return self.page  # dışarıya "page" döndürüyoruz
+        self.page = await self.browser.new_page()
+        return self.page
 
     async def __aexit__(self, exc_type, exc, tb):
-        await self.context.close()
         await self.browser.close()
         await self.playwright.stop()
