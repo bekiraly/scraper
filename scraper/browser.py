ec65626164fb0f56a27c2b31fc6b1b0b7e23c832
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-12-03 21:19:29 +0300

    Update browser.py

--- scraper/browser.py
+++ scraper/browser.py
@@ -3,15 +3,20 @@ from playwright.async_api import async_playwright
 class Browser:
     async def __aenter__(self):
         self.playwright = await async_playwright().start()
+
+        # Railway içinde tarayıcı buraya iniyor
+        chrome_path = "/tmp/pw/chromium/chrome-linux/chrome"
+
         self.browser = await self.playwright.chromium.launch(
             headless=True,
+            executable_path=chrome_path,
             args=[
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
                 "--disable-dev-shm-usage"
-            ],
-            executable_path="/tmp/pw/chromium/chrome-linux/chrome"
+            ]
         )
+
         self.page = await self.browser.new_page()
         return self.page
 
